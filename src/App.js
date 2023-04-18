352bb14e6331c13a9671dd18e0242e2644b532ee
retrofuture <kiril.yanakiev.a20@learn.telerikacademy>
2023-04-18 15:27:01 +0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <table>
+      <tr>
+        <th>id</th>
+        <th>status</th>
+        <th>created_at</th>
+        <th>merchant_name</th>
+        <th>type</th>
+        <th>error_class</th>
+        <th>card_holder</th>
+        <th>card_number</th>
+        <th>amount</th>
+      </tr>
+    </table>
+  );
+};
+
+export default App;
