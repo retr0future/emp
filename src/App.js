8b9a67fe8b50df92383d7f1d8277549d819edc42
retrofuture <kiril.yanakiev.a20@learn.telerikacademy>
2023-04-19 14:56:40 +0300

    Added table sorting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 import "./App.css";
 import PaymentTransactions from "./PaymentTransactions.js";
+
 const App = () => {
   const paymentTransactions = new PaymentTransactions();
   const transactions = paymentTransactions.getTransactions();
+  const [order, setOrder] = useState("asc");
+  const [data, setData] = useState(transactions);
+  const handleSort = (col) => {
+    if (order === "asc") {
+      const sorted = [...data].sort((a, b) => (a[col] > b[col] ? 1 : -1));
+      setData(sorted);
+      setOrder("desc");
+    }
+    if (order === "desc") {
+      const sorted = [...data].sort((a, b) => (a[col] < b[col] ? 1 : -1));
+      setData(sorted);
+      setOrder("asc");
+    }
+  };
   const errorClassMap = {
     'Module::SystemError': 'System',
     'Module::RemoteError': 'Remote',
@@ -14,18 +29,18 @@ const App = () => {
       <thead>
         <tr className="headRow">
           <th>ID</th>
-          <th>Status</th>
-          <th>Created At</th>
-          <th>Merchant Name</th>
-          <th>Type</th>
-          <th>Error Class</th>
-          <th>Card Holder</th>
-          <th>Card Number</th>
-          <th>Amount</th>
+          <th onClick={() => handleSort('status')}>Status</th>
+          <th onClick={() => handleSort('created_at')}>Created At</th>
+          <th onClick={() => handleSort('merchant_name')}>Merchant Name</th>
+          <th onClick={() => handleSort('type')}>Type</th>
+          <th onClick={() => handleSort('error_class')}>Error Class</th>
+          <th onClick={() => handleSort('card_holder')}>Card Holder</th>
+          <th onClick={() => handleSort('card_number')}>Card Number</th>
+          <th onClick={() => handleSort('amount')}>Amount</th>
         </tr>
       </thead>
       <tbody>
-        {transactions.map(transaction => (
+        {data.map(transaction => (
           <tr key={transaction.id}>
             <td>{transaction.id}</td>
             <td>{transaction.status}</td>
